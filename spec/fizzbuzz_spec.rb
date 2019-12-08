20e8bc39d104043ebf04b3033f51cad8c02f3ba2
AliciaMerlino <acmerlino1@gmail.com>
2019-12-08 14:48:04 +0000

    fizzbuzz programme and tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,22 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when passed multiple of 3' do
+    [3, 6, 9, 12].each do |i|
+      expect(fizzbuzz(i)).to eq 'fizz'
+    end
+  end
+  it 'returns "buzz" when passed multiple of 5' do
+    [5, 10].each do |i|
+      expect(fizzbuzz(i)).to eq 'buzz'
+    end
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns the number otherwise' do
+    [1, 2, 4, 7, 8, 11, 13, 14].each do |i|
+      expect(fizzbuzz(i)).to eq i
+    end
   end
 end
